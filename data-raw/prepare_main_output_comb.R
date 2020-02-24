6750784bcf0a134adaab93799be7d2c5a8955ea6
michielvandijk <michiel.vandijk@wur.nl>
2020-02-24 11:57:40 +0100

    Add NEWS

--- data-raw/prepare_main_output_comb.R
+++ data-raw/prepare_main_output_comb.R
@@ -3,24 +3,12 @@
 -------------------------------------------------------------------------------------------------
 
 ############### PACKAGES ###############
-library(readxl)
+library(readr)
+library(rglobiom)
 
 ############### LOAD DATA ###############
-# Load data and combine scenario data
-eu_nuts_pdf_raw <- bind_rows(
-  read_excel("E:/OneDrive - IIASA/projects/rglobiom/data/PDF_ha_10-12.xlsx", sheet = "BASE") %>%
-    mutate(scenario = "BASE"),
-  read_excel("E:/OneDrive - IIASA/projects/rglobiom/data/PDF_ha_10-12.xlsx", sheet = "BASE") %>%
-    mutate(scenario = "CONST"),
-  read_excel("E:/OneDrive - IIASA/projects/rglobiom/data/PDF_ha_10-12.xlsx", sheet = "BASE") %>%
-    mutate(scenario = "EMIRED"))
-
+main_output_comb <- read_csv(system.file("globiom", "main_output_comb.csv", package = "rglobiom"))
 
 ############### PROCESS ###############
-# Put in long format
-eu_nuts_pdf <- eu_nuts_pdf_raw %>%
-  gather(year, value, -NUTS2, -scenario) %>%
-  mutate(year = as.integer(year))
-
 # Add as data to package
-usethis::use_data(eu_nuts_pdf, overwrite = T)
+usethis::use_data(main_output_comb, overwrite = T)
