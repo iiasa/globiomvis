01bb4c75e1b047da7ec618131edcbc1d0e7fd172
Albert Brouwer <brouwer@iiasa.ac.at>
2020-02-28 17:16:43 +0100

    Drop test that fails with R CMD check
    Getting the root path sorted for all test contexts is adding too much complexity, even when using rprojroot.

--- tests/testthat/test-load-package.R
+++ tests/testthat/test-load-package.R
@@ -1,10 +1,3 @@
 test_that("globiomvis is loaded for test", {
   expect_true("globiomvis" %in% loadedNamespaces())
 })
-
-test_that("package loads and detaches", {
-  expect_error(devtools::load_all(".", reset=TRUE), NA)
-  expect_true("globiomvis" %in% loadedNamespaces())
-  expect_error(detach("package:globiomvis", unload=TRUE), NA)
-  expect_false("globiomvis" %in% loadedNamespaces())
-})
