87d705bfb4fb54d66ceb5e2049612d04142895b6
Albert Brouwer <brouwer@iiasa.ac.at>
2020-03-25 10:43:51 +0100

    Add documentation for region30_country data

---
+++ R/region30_country.R
@@ -0,0 +1,12 @@
+#' GLOBIOM 30-region to country mapping.
+#'
+#' @format A data frame with 182 rows and 5 variables:
+#' \describe{
+#'   \item{ANYREGION}{GLOBIOM region label}
+#'   \item{ALLCOUNTRY}{GLOBIOM country label}
+#'   \item{iso3c}{ISO 3-letter country code}
+#'   \item{country.name}{Country name}
+#'   \item{GLOBIOM}{'y' for GLOBIOM-originated}
+#' }
+#' @source GLOBIOM
+"region30_country"
